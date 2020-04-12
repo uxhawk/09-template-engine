69a525ec245b9104859ac2fb8b1283a9fd4f4f58
David Hawkins <uxhawk@gmail.com>
2020-04-12 18:27:49 -0400

    can instantiate each employee class/sub class

--- index.js
+++ index.js
@@ -1,48 +1,53 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const Employee = require("./lib/Employee");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
 
-function promptUser() {
-    return inquirer.prompt([{
-            type: "input",
-            name: "mgrName",
-            message: "First up, we're going to start with the manager. What is the manager's name?"
-        },
-        {
-            type: "input",
-            name: "projTitle",
-            message: "What is the title of your project?"
-        },
-        {
-            type: "input",
-            name: "projDesc",
-            message: "What is the description of your project?"
-        },
-        {
-            type: "input",
-            name: "projInstall",
-            message: "What are the installation instructions?"
-        },
-        {
-            type: "input",
-            name: "projUsage",
-            message: "What are the usage instructions?"
-        },
-        {
-            type: "list",
-            name: "license",
-            message: "What license would you like to use?",
-            choices: ["ISC", "MIT", "GNU GPLv3"]
-        },
-        {
-            type: "input",
-            name: "projTests",
-            message: "What are the testing instructions?"
-        },
-        {
-            type: "input",
-            name: "projQuestions",
-            message: "What are the questions for this project?"
-        },
 
-    ]);
-}
+// function promptUser() {
+//     return inquirer.prompt([{
+//             type: "input",
+//             name: "mgrName",
+//             message: "First up, we're going to start with the manager. What is the manager's name?"
+//         },
+//         {
+//             type: "input",
+//             name: "projTitle",
+//             message: "What is the title of your project?"
+//         },
+//         {
+//             type: "input",
+//             name: "projDesc",
+//             message: "What is the description of your project?"
+//         },
+//         {
+//             type: "input",
+//             name: "projInstall",
+//             message: "What are the installation instructions?"
+//         },
+//         {
+//             type: "input",
+//             name: "projUsage",
+//             message: "What are the usage instructions?"
+//         },
+//         {
+//             type: "list",
+//             name: "license",
+//             message: "What license would you like to use?",
+//             choices: ["ISC", "MIT", "GNU GPLv3"]
+//         },
+//         {
+//             type: "input",
+//             name: "projTests",
+//             message: "What are the testing instructions?"
+//         },
+//         {
+//             type: "input",
+//             name: "projQuestions",
+//             message: "What are the questions for this project?"
+//         },
+
+//     ]);
+// }
